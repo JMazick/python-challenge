5b49ab79db86f0d36a2ad53d750f113f15220a39
JMazick <jazlynmm@outlook.com>
2020-10-21 19:04:39 -0500

    Updated main python script

--- PyBank/main.py
+++ PyBank/main.py
@@ -7,14 +7,40 @@ os.chdir("C:\\Users\\jazly\\OneDrive\\BOOTCAMP\\HOMEWORK\\WEEK-3")
 # Define the function and have it accept the 'PyBank' as its sole parameter
 PyBank = "00_Homework Assignments Instructions & Data_Week3_Instructions_PyBank_Resources_budget_data.csv"
 
+Date = []
+ProfitLosses = []
+ProfitChange= []
+
 with open(PyBank) as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=",")
     csv_header = next(csv_file)
     print(f"Header: {csv_header}")
     
     for row in csv_reader:
-        Date = row[0]
-        ProfitLosses = row[1]
+        Date.append(row[0])
+        ProfitLosses.append(int(row[1]))
         
+    for x in range(len(ProfitLosses) - 1):
+        ProfitChange.append(ProfitLosses[x + 1] - ProfitLosses[x])
+        
+        
+        
+print(Date)
 
-#pybank into a dictionary???
+print(len(Date))
+
+print(ProfitLosses)
+
+print(sum(ProfitLosses))
+
+TotalMonths = len(Date)
+TotalProfits = sum(ProfitLosses)
+
+print(TotalMonths)
+print(TotalProfits)
+
+
+
+AverageChange = (sum(ProfitChange) / (len(ProfitChange)))
+print(AverageChange)
+
