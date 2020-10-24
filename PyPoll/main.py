c279e739f7b8d1c867213d93adec704d39325538
JMazick <jazlynmm@outlook.com>
2020-10-24 18:32:18 -0500

    Update PyPoll script

---
+++ PyPoll/main.py
@@ -0,0 +1,23 @@
+import csv
+import os
+
+# Path to collect date from the Resources folder
+os.chdir("C:\\Users\\jazly\\OneDrive\\Desktop\\HOMEWORK SUBMISSION\\WEEK 3\\python-challenge\\PyPoll\\Resources")
+
+# Define the function and have it accept the 'PyPoll' as its sole parameter
+PyPoll = "00_Homework Assignments Instructions & Data_Week3_Instructions_PyPoll_Resources_election_data.csv"
+
+with open(PyPoll) as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=",")
+    csv_header = next(csv_file)
+    print(f"Header: {csv_header}")
+    
+# Define lists
+Voter_ID = []
+County = []
+Candidate = []
+  
+for row in csv_reader:
+    Voter_ID.append(int(row[0]))
+    County.append(row[1])
+    Candidate.append(row[2])
